3379ad44eb0c32642a6710e22a639462fd4e4f48
WAWBS <493847964@qq.com>
2020-11-03 21:26:31 +0800

    家庭作业commit

---
+++ 家庭作业/date.c
@@ -0,0 +1,9 @@
+//ÈÕÆÚ×ª»» 
+#include<stdio.h>
+int main()
+{int a,b,c;
+printf("Enter a date(mm/dd/yyyy):",a,b,c);
+ scanf("%d/%d/%d",&a,&b,&c);
+ printf("You entered the date:%d %d %d",c,a,b);
+ return 0;
+ } 
