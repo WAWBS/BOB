f63c3740ab58900abe886df4289be3fcbf954298
WAWBS <493847964@qq.com>
2021-01-09 20:34:53 +0800

    123

---
+++ homework/小胖的表.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<string.h>
+int main(void){
+	int format[999][999];
+	int i=0,t=0,row,column;
+	char order[999][999]
+	scanf("%d%d",&row,&column);
+	for(i=0;i<row;i++){
+		for(t=0;t<column;t++){
+		scanf("%d",&format[i][t]);
+	}
+	for(j=0;j<d;j++){
+		scanf("%s",order[i]);
+	}
+	if(order[i])
+	}
+	return 0;
+	
+} 
+
